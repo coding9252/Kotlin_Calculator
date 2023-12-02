39a71a4805387de6a967cc2d18683c239a8a8248
Gahyeon <coding9252@naver.com>
2023-12-02 20:32:16 +0900

    re

---
+++ app/src/main/java/com/leegahyeon/kotlin_calculator/AddOperation.kt
@@ -0,0 +1,4 @@
+package com.leegahyeon.kotlin_calculator
+
+class AddOperation {
+}
--- app/src/main/java/com/leegahyeon/kotlin_calculator/main.kt
+++ app/src/main/java/com/leegahyeon/kotlin_calculator/main.kt
@@ -1,40 +1,7 @@
 package com.leegahyeon.kotlin_calculator
 
 fun main() {
-    var num1 = readLine()!!.toDouble()
+    // 더하기
+    val addCalc = Calculator(AddOperation())
 
-
-    while (true) {
-        val operator = readLine()!!.toString()
-        val num2 = readLine()!!.toDouble()
-
-        when (operator) {
-            "+" -> {
-                num1 = num1 + num2
-                println("${num1}")
-            }
-
-            "-" -> {
-                num1 = num1 - num2
-                println("${num1}")
-            }
-
-            "*" -> {
-                num1 = num1 * num2
-                println("${num1}")
-            }
-
-            "/" -> {
-                num1 = num1 / num2
-                println("${num1}")
-            }
-
-            "%" -> {
-                num1 = num1 % num2
-                println("${num1}")
-            }
-
-            else -> println("잘못 입력하셨습니다.")
-        }
-    }
 }
